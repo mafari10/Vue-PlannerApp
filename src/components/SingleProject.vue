fd76be5b20ce4d5d704c9792aa84494398d537a0
Michael Afari <michealnewtons419@gmail.com>
2024-11-28 21:22:20 +0100

    Project setup and api local integration

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="project">
+    <div class="actions">
+      <h3 class="text-xl font-bold text-red-300" @click="show = !show">
+        {{ project.title }}
+      </h3>
+      <div class="icons">
+        <span class="material-icons">edit</span>
+        <span class="material-icons">delete</span>
+        <span class="material-icons">done</span>
+      </div>
+    </div>
+    <div class="details" v-if="show">
+      <p>{{ project.details }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["project"],
+  data() {
+    return {
+      show: true,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.project {
+  margin: 20px auto;
+  background: white;
+  padding: 20px 10px;
+  border-radius: 9px;
+  box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.5);
+  border-left: 6px solid rgb(182, 115, 126);
+}
+h3 {
+  cursor: pointer;
+}
+.actions {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.material-icons {
+  font-size: 14px;
+  margin-left: 10px;
+  color: #bbb;
+  cursor: pointer;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,35 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <SingleProject :project="project" />
+      </div>
+    </div>
+  </div>
 </template>
+
+<script >
+import axios from "axios";
+import SingleProject from "../components/SingleProject.vue";
+export default {
+  name: "Home",
+  components: { SingleProject },
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  async created() {
+    try {
+      const response = await axios.get("http://localhost:3000/projects");
+      this.projects = response.data;
+      console.log(this.projects);
+    } catch (e) {
+      console.error("Error getting", e);
+    }
+  },
+};
+</script>
+<style scoped>
+</style>
+
