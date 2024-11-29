62561a98c442eaacfa79bfa3201e274bb5f09aa9
Michael Afari <michealnewtons419@gmail.com>
2024-11-29 01:01:12 +0100

    Structured code

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,5 @@
 <script>
 </script>
 
-<style scopeed>
-body {
-  background: #f2f2f2;
-}
-#app {
-  font-family: Avenir, Arial, Helvetica, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  max-width: 600px;
-  margin: auto;
-  color: #555;
-}
+<style scoped>
 </style>
